7d429fe0db0a7183c24fc4ac5f3e00fbd526cadc
ciulugbek <ciulugbek@list.ru>
2022-08-22 12:05:51 +0500

    commit8

--- project/settings.py
+++ project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sdca9&5o*#k@bplpc8jw1mw60ex7b5rh4qd*o16ey$pm3eii)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['geniusproject1.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['geniusproject1.herokuapp.com', '127.0.0.1','*']
 
 
 # Application definition
